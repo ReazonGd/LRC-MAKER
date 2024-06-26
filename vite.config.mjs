6f78730e742de33a1afaf16c51642f083e36c4e6
Reazon Gd <68423132+ReazonGd@users.noreply.github.com>
2024-06-26 10:18:30 +0700

    fak

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,10 @@
 import { copy } from "vite-plugin-copy";
 
 export default {
-  plugins: [copy([{ src: "./language/", dest: "./dist/" }], [{ src: "./asset/", dest: "./dist/" }])],
+  plugins: [
+    copy([
+      { src: "./language/", dest: "./dist/" },
+      { src: "./asset-image/", dest: "./dist/" },
+    ]),
+  ],
 };
