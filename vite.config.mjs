783f398008da64996bc08ab082941d06e3048f31
Reazon CP <cah.gaming707@gmail.com>
2025-11-26 01:33:45 +0700

    remove unesecery plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,3 @@
 import { defineConfig } from "vite";
-import { copy } from "vite-plugin-copy";
 
-export default defineConfig({
-  plugins: [copy([{ src: "./asset-image/", dest: "./dist/" }]), copy([{ src: "./language/", dest: "./dist/" }])],
-});
+export default defineConfig({});
